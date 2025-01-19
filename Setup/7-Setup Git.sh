0220c4097f2c58ef5df1ecdc0fbba081fd8edf3f
aollivierre <abdullahollivierre@gmail.com>
2025-01-18 16:28:11 -0800

    Create 7-Setup Git.sh

---
+++ Setup/7-Setup Git.sh
@@ -0,0 +1,8 @@
+# Set your Git email (use the same email as your GitHub account)
+git config --global user.email "AbdullahOllivierre@gmail.com"
+
+# Set your Git name
+git config --global user.name "aollivierre"
+
+# Verify the settings
+git config --global --list
